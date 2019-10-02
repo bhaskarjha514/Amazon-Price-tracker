1a48bb9139468aa0e8dc5954f1ab5d187de6ccea
bhaskarjha514 <bhaskarjha514>
2019-10-02 10:59:12 +0530

    updated

--- scrap.py
+++ scrap.py
@@ -1,8 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-
-url = 'https://www.amazon.in/dp/B07DJHR5DY/ref=psdc_1805560031_t1_B07P72FGSY2'
+url = 'https://www.amazon.in/Test-Exclusive-743/dp/B07DJHWWLN/ref=sr_1_1_sspa?crid=2ULP7O05QLR22&keywords=mi+note+7+pro+mobile+phone&qid=1569993804&s=gateway&smid=A14CZOWI0VEHLG&sprefix=mi+no%2Caps%2C1142&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzMU1JVkQ0V1JVQkVQJmVuY3J5cHRlZElkPUEwMjY1NzM5MkJTWjBMVDhTWlpMMSZlbmNyeXB0ZWRBZElkPUEwMDU4ODk4MUYwUlo5T1pTTUcxQiZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU='
 source = requests.get(url).text
 soup = BeautifulSoup(source, 'lxml')
 
@@ -12,20 +11,6 @@ title=title.strip()
 image = soup.find(id='landingImage')
 img_name = image.get('src')
 
-
-
-
-
-EarlierPrice = soup.find(class_='priceBlockStrikePriceString a-text-strike').get_text()
-EarlierPrice = EarlierPrice.replace(',','')
-# price = soup.find(id='priceblock_dealprice').get_text() or soup.find(id='priceblock_ourprice').get_text()
-# price = price.replace(',','')
-# price = float(price[2:])
-# print("Product_price:", price)
-
-
-EarlierPrice = float(EarlierPrice[2:])
-
 print("\nProduct Name :",title)
 try:
     price = soup.find(id='priceblock_dealprice').get_text() or soup.find(id='priceblock_ourprice').get_text()
@@ -34,8 +19,14 @@ try:
     print("Product_price:", price)
 except AttributeError:
     pass
+try:
+    EarlierPrice = soup.find(class_='priceBlockStrikePriceString a-text-strike').get_text()
+    EarlierPrice = EarlierPrice.replace(',','')
+    EarlierPrice = float(EarlierPrice[2:])
+    print("Earlier Price:", EarlierPrice)
+except AttributeError:
+    pass
 
 print(img_name)
-print("EarlierPrice:",EarlierPrice)
-# print(EarlierPrice)
+
    
